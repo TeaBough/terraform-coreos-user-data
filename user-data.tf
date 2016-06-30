1d3c8003d5856444f49954d63afb67954d03fa45
Thibault Vigouroux <thibault.vigouroux@gmail.com>
2016-06-30 17:59:11 +0200

    Auto signing

--- user-data.tf
+++ user-data.tf
@@ -2,7 +2,7 @@ resource "template_file" "user-data" {
   template = "${file("${path.module}/templates/user-data.yml")}"
 
   provisioner "local-exec" {
-    command = "curl -s 'https://validate.core-os.net/validate' -X PUT -H 'Content-Type: text/plain' -H 'Accept: application/json' --data-binary @- <<'__USERDATA__' | tee /dev/stderr | grep --invert-match -E '(error|warning)' > /dev/null\n${template_file.user-data.rendered}\n__USERDATA__"
+    command = "curl -k -s 'https://validate.core-os.net/validate' -X PUT -H 'Content-Type: text/plain' -H 'Accept: application/json' --data-binary @- <<'__USERDATA__' | tee /dev/stderr | grep --invert-match -E '(error|warning)' > /dev/null\n${template_file.user-data.rendered}\n__USERDATA__"
   }
 
   vars {
